a50606fb6f8c4d869d18b33775ea6af35dcf48d2
Anthony Lee <anthonylee5519@gmail.com>
2024-12-10 14:13:05 -0700

    schema done

---
+++ tower_node/src/models/TowerEvent.js
@@ -0,0 +1,18 @@
+import { referrerPolicy } from "helmet";
+import { Schema } from "mongoose";
+
+export const TowerSchema = new Schema({
+  creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+  name: { type: String, minLength: 3, maxLength: 50, required: true },
+  description: { type: String, minLength: 15, maxLength: 1000, required: true },
+  coverImg: { type: String, maxLength: 1000, required: true },
+  location: { type: String, minLength: 1, maxLength: 500, required: true },
+  capacity: { type: Number, min: 1, max: 5000, required: true },
+  startDate: { type: Date, required: true },
+  isCanceled: { type: Boolean, required: true },
+  type: { type: String, enum: ['concert', 'convention', 'sport', 'digital'], required: true }
+},
+  {
+    timestamps: true,
+    toJSON: { virtuals: true }
+  })
